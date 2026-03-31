c1b7af3dc2b9bfbbacccf02045adbba84f81b703
Sébastien Graveline <graveline.seb@gmail.com>
2026-03-31 16:49:16 -0400

    add clipboard support

--- internal/counter/tui/update.go
+++ internal/counter/tui/update.go
@@ -10,6 +10,7 @@ import (
 
 	tea "charm.land/bubbletea/v2"
 
+	"github.com/atotto/clipboard"
 	"github.com/boostsecurityio/smokedmeat/internal/counter"
 	"github.com/boostsecurityio/smokedmeat/internal/pantry"
 	"github.com/boostsecurityio/smokedmeat/internal/rye"
@@ -1431,7 +1432,7 @@ func (m Model) handleKeyMsg(msg tea.KeyPressMsg) (tea.Model, tea.Cmd) {
 		if m.paneFocus == PaneFocusLoot {
 			secret := m.SelectedLootSecret()
 			if secret != nil {
-				if err := copyToClipboard(secret.Value); err != nil {
+				if err := clipboard.WriteAll(secret.Value); err != nil {
 					m.activityLog.Add(IconError, fmt.Sprintf("Copy failed: %v", err))
 				} else {
 					m.lootFlash = true
